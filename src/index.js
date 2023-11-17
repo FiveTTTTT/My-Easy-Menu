1236d1c92bc9e8a9854c4fc5740452d090537d35
Delafosse Alexandre <alex.delaf@outlook.fr>
2023-11-17 16:59:42 +0100

    tre

--- src/index.js
+++ src/index.js
@@ -44,40 +44,43 @@ class MenuBurger extends HTMLElement {
     connectedCallback() {
         // Création du conteneur global
         const containerDiv = document.createElement('div');
-
+    
         // Ajout des trois spans dans le conteneur global
         const span1 = document.createElement('span');
         const span2 = document.createElement('span');
         const span3 = document.createElement('span');
-
+    
         containerDiv.appendChild(span1);
         containerDiv.appendChild(span2);
         containerDiv.appendChild(span3);
-
+    
         // Ajout du conteneur global à l'élément menu-burger
         containerDiv.className = 'trigger';
         this.appendChild(containerDiv);
-
+    
         // Création du conteneur ul
         const menuBurgerDiv = document.createElement('ul');
-        menuBurgerDiv.className = 'menu-burger';
-
-        this.createStyle();
-
+    
+        // Ajoutez la classe directement à l'élément menu-burger
+        var positionselector = this.getAttribute("position");
+        menuBurgerDiv.classList.add("menu-burger", positionselector);
+    
         // Séparation des li avec la classe menu-burger-item des autres éléments
         const menuBurgerItems = Array.from(this.querySelectorAll('li.menu-burger-item'));
-
+    
         // Ajout des clones des li avec la classe menu-burger-item dans le conteneur ul
         menuBurgerItems.forEach(item => menuBurgerDiv.appendChild(item.cloneNode(true)));
-
+    
         // Ajout du conteneur ul à l'élément menu-burger
         this.appendChild(menuBurgerDiv);
-
+    
+        this.createStyle();
+    
         // Ajout d'un gestionnaire d'événements pour le clic sur l'élément avec la classe 'trigger'
         containerDiv.addEventListener('click', () => {
             menuBurgerDiv.classList.toggle('open');
         });
-
+    
         // Ajout d'un gestionnaire d'événements pour le clic en dehors de l'élément
         document.addEventListener('click', (event) => {
             const isInsideMenuBurger = this.contains(event.target) || menuBurgerDiv.contains(event.target);
@@ -85,8 +88,7 @@ class MenuBurger extends HTMLElement {
                 menuBurgerDiv.classList.remove('open');
             }
         });
-    }
-
+    }    
 
 }
 

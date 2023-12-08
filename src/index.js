09bb78fb7af7dfc0754f736c1be7a3c1ee26f6dc
Delafosse Alexandre <alex.delaf@outlook.fr>
2023-12-08 11:09:58 +0100

    trigger movements

--- src/index.js
+++ src/index.js
@@ -4,17 +4,20 @@ class MenuBurger extends HTMLElement {
         const style = document.createElement('style');
         style.type = 'text/css';
         let menuBurgerStyle = '.menu-burger {';
-        let menuBurgerLinkStyle = '.menu-burger a {'
+        let triggerStyle = '.trigger {';
+        let menuBurgerLinkStyle = '.menu-burger a {';
         style.textContent = '';
-
+    
         // Positionnement du menu
         const position = this.getAttribute('position') || '';
         switch (position) {
             case 'left':
                 menuBurgerStyle = menuBurgerStyle + 'top: 0; ';
-            break;
+                break;
             case 'right':
                 menuBurgerStyle = menuBurgerStyle + 'right: 0; top: 0; ';
+                triggerStyle = triggerStyle + 'align-items: flex-end;';
+
             break;
             case 'top':
                 menuBurgerStyle = menuBurgerStyle + 'top: 0; ';
@@ -43,61 +46,78 @@ class MenuBurger extends HTMLElement {
         }
 
         menuBurgerStyle += '}';
+        
+    triggerStyle += '}';
         menuBurgerLinkStyle += '}';
 
 
-        style.textContent += menuBurgerStyle + menuBurgerLinkStyle + '}';
+        style.textContent += menuBurgerStyle + triggerStyle + menuBurgerLinkStyle + '}';
         document.body.appendChild(style);
     }
 
-    connectedCallback() {
-        // Création du conteneur global
-        const containerDiv = document.createElement('div');
+connectedCallback() {
+    // Création du conteneur global
+    const containerDiv = document.createElement('div');
     
-        // Ajout des trois spans dans le conteneur global
-        const span1 = document.createElement('span');
-        const span2 = document.createElement('span');
-        const span3 = document.createElement('span');
+    // Ajout des trois spans dans le conteneur global
+    const span1 = document.createElement('span');
+    const span2 = document.createElement('span');
+    const span3 = document.createElement('span');
     
-        containerDiv.appendChild(span1);
-        containerDiv.appendChild(span2);
-        containerDiv.appendChild(span3);
+    containerDiv.appendChild(span1);
+    containerDiv.appendChild(span2);
+    containerDiv.appendChild(span3);
     
-        // Ajout du conteneur global à l'élément menu-burger
-        containerDiv.className = 'trigger';
-        this.appendChild(containerDiv);
+    // Ajout du conteneur global à l'élément menu-burger
+    containerDiv.className = 'trigger';
+    this.appendChild(containerDiv);
     
-        // Création du conteneur ul
-        const menuBurgerDiv = document.createElement('ul');
+    // Création du conteneur ul
+    const menuBurgerDiv = document.createElement('ul');
     
-        // Ajoutez la classe directement à l'élément menu-burger
-        var positionselector = this.getAttribute("position");
-        menuBurgerDiv.classList.add("menu-burger", positionselector);
+    // Ajout de la classe directement à l'élément menu-burger
+    const positionSelector = this.getAttribute("position");
+    menuBurgerDiv.classList.add("menu-burger", positionSelector);
     
-        // Séparation des li avec la classe menu-burger-item des autres éléments
-        const menuBurgerItems = Array.from(this.querySelectorAll('li.menu-burger-item'));
+    // Séparation des li avec la classe menu-burger-item des autres éléments
+    const menuBurgerItems = Array.from(this.querySelectorAll('li.menu-burger-item'));
     
-        // Ajout des clones des li avec la classe menu-burger-item dans le conteneur ul
-        menuBurgerItems.forEach(item => menuBurgerDiv.appendChild(item.cloneNode(true)));
+    // Ajout des clones des li avec la classe menu-burger-item dans le conteneur ul
+    menuBurgerItems.forEach(item => menuBurgerDiv.appendChild(item.cloneNode(true)));
     
-        // Ajout du conteneur ul à l'élément menu-burger
-        this.appendChild(menuBurgerDiv);
+    // Création de la div autour du ul
+    const menuContainerDiv = document.createElement('div');
+    menuContainerDiv.classList.add('menu-container');
+    menuContainerDiv.appendChild(menuBurgerDiv);
     
-        this.createStyle();
+    // Ajout du conteneur div à l'élément menu-burger
+    this.appendChild(menuContainerDiv);
     
-        // Ajout d'un gestionnaire d'événements pour le clic sur l'élément avec la classe 'trigger'
-        containerDiv.addEventListener('click', () => {
-            menuBurgerDiv.classList.toggle('open');
-        });
+    // Création du déclencheur pour fermer le menu
+    const closeTrigger = document.createElement('div');
+    closeTrigger.className = 'close-trigger';
+    this.appendChild(closeTrigger);
     
-        // Ajout d'un gestionnaire d'événements pour le clic en dehors de l'élément
-        document.addEventListener('click', (event) => {
-            const isInsideMenuBurger = this.contains(event.target) || menuBurgerDiv.contains(event.target);
-            if (!isInsideMenuBurger) {
-                menuBurgerDiv.classList.remove('open');
-            }
-        });
-    }    
+    this.createStyle();
+    
+    // Ajout d'un gestionnaire d'événements pour le clic sur l'élément avec la classe 'trigger'
+    containerDiv.addEventListener('click', () => {
+        menuBurgerDiv.classList.toggle('open');
+    });
+    
+    // Ajout d'un gestionnaire d'événements pour le clic en dehors de l'élément
+    document.addEventListener('click', (event) => {
+        const isInsideMenuBurger = this.contains(event.target) || menuBurgerDiv.contains(event.target);
+        if (!isInsideMenuBurger) {
+            menuBurgerDiv.classList.remove('open');
+        }
+    });
+    
+    // Ajout d'un gestionnaire d'événements pour le clic sur le déclencheur de fermeture
+    closeTrigger.addEventListener('click', () => {
+        menuBurgerDiv.classList.remove('open');
+    });
+} 
 
 }
 

3d5858b157d77414a5b8aba82434091ebcee71cd
Théodore Cadet <cadetthe@gmail.com>
2023-12-08 11:58:50 +0100

    wip : close trigger in menu burger

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ class MenuBurger extends HTMLElement {
         const style = document.createElement('style');
         style.type = 'text/css';
         let menuBurgerStyle = '.menu-burger {';
-        let menuBurgerLinkStyle = '.menu-burger a {'
+        let menuBurgerLinkStyle = '.menu-burger a {';
         style.textContent = '';
 
         // Positionnement du menu
@@ -12,22 +12,23 @@ class MenuBurger extends HTMLElement {
         switch (position) {
             case 'left':
                 menuBurgerStyle = menuBurgerStyle + 'top: 0; ';
-            break;
+                break;
             case 'right':
                 menuBurgerStyle = menuBurgerStyle + 'right: 0; top: 0; ';
-            break;
+                this.style = 'display: flex; flex-direction: row-reverse;';
+                break;
             case 'top':
                 menuBurgerStyle = menuBurgerStyle + 'top: 0; ';
-            break;
+                break;
             case 'bottom':
                 menuBurgerStyle = menuBurgerStyle + 'bottom: 0;';
 
-            break;
+                break;
             case 'full':
-                menuBurgerStyle = menuBurgerStyle + 'top: 0; ' ;
+                menuBurgerStyle = menuBurgerStyle + 'top: 0; ';
             default:
-            
-            break;
+
+                break;
         }
 
 
@@ -53,43 +54,63 @@ class MenuBurger extends HTMLElement {
     connectedCallback() {
         // Création du conteneur global
         const containerDiv = document.createElement('div');
-    
+
         // Ajout des trois spans dans le conteneur global
         const span1 = document.createElement('span');
         const span2 = document.createElement('span');
         const span3 = document.createElement('span');
-    
+
         containerDiv.appendChild(span1);
         containerDiv.appendChild(span2);
         containerDiv.appendChild(span3);
-    
+
         // Ajout du conteneur global à l'élément menu-burger
         containerDiv.className = 'trigger';
         this.appendChild(containerDiv);
-    
+
         // Création du conteneur ul
-        const menuBurgerDiv = document.createElement('ul');
-    
+        const menuBurgerDiv = document.createElement('div');        
+
+
+        // Création du conteneur global
+        const containerDiv2 = document.createElement('div');
+
+        // Ajout des trois spans dans le conteneur global
+        const span21 = document.createElement('span');
+        const span22 = document.createElement('span');
+        const span23 = document.createElement('span');
+
+        containerDiv2.appendChild(span21);
+        containerDiv2.appendChild(span22);
+        containerDiv2.appendChild(span23);
+
+        // Ajout du conteneur global à l'élément menu-burger
+        containerDiv2.className = 'trigger closeBurger';
+        menuBurgerDiv.appendChild(containerDiv2);
+
+        const menuBurgerUl = document.createElement('ul');
+        menuBurgerDiv.appendChild(menuBurgerUl);
+
         // Ajoutez la classe directement à l'élément menu-burger
         var positionselector = this.getAttribute("position");
         menuBurgerDiv.classList.add("menu-burger", positionselector);
-    
+
         // Séparation des li avec la classe menu-burger-item des autres éléments
         const menuBurgerItems = Array.from(this.querySelectorAll('li.menu-burger-item'));
-    
+
         // Ajout des clones des li avec la classe menu-burger-item dans le conteneur ul
-        menuBurgerItems.forEach(item => menuBurgerDiv.appendChild(item.cloneNode(true)));
-    
+        menuBurgerItems.forEach(item => menuBurgerUl.appendChild(item.cloneNode(true)));
+
         // Ajout du conteneur ul à l'élément menu-burger
         this.appendChild(menuBurgerDiv);
-    
+
         this.createStyle();
-    
+
         // Ajout d'un gestionnaire d'événements pour le clic sur l'élément avec la classe 'trigger'
         containerDiv.addEventListener('click', () => {
             menuBurgerDiv.classList.toggle('open');
         });
-    
+
         // Ajout d'un gestionnaire d'événements pour le clic en dehors de l'élément
         document.addEventListener('click', (event) => {
             const isInsideMenuBurger = this.contains(event.target) || menuBurgerDiv.contains(event.target);
@@ -97,7 +118,7 @@ class MenuBurger extends HTMLElement {
                 menuBurgerDiv.classList.remove('open');
             }
         });
-    }    
+    }
 
 }
 
